bac4a39d924b393f5cc3ac599029c9f192af0c87
Namelessafk <marcusthuliok9@gmail.com>
2021-02-28 13:26:31 -0300

    Gerando componentes da para o To-do

---
+++ src/app/components/to-dos/to-dos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-to-dos',
+  templateUrl: './to-dos.component.html',
+  styleUrls: ['./to-dos.component.css']
+})
+export class ToDosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
